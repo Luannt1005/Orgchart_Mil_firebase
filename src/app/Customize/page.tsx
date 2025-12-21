53a205e3b70ba721d0c19b0c3ca99a95467c2134
Luannt1005 <100223397+Luannt1005@users.noreply.github.com>
2025-12-21 17:16:29 +0700

    test power apps

--- src/app/Customize/page.tsx
+++ src/app/Customize/page.tsx
@@ -1,9 +1,19 @@
-const Youtube = () => {
-    return(
-        <div>
-            Youtube page
+const Customize = () => {
+    return (
+        <div style={{ width: "100%", height: "100vh" }}>
+            <iframe
+                src="https://apps.powerapps.com/play/e/default-8b8cc6cf-0eaa-4b64-98e4-6d4672c44930/a/86bb2f70-40a6-4cfe-a190-22cec40241d2?tenantId=8b8cc6cf-0eaa-4b64-98e4-6d4672c44930&hint=8450905d-8e22-4d91-a6b7-d91c6da49721&sourcetime=1766310676065"
+                style={{
+                    width: "100%",
+                    height: "100%",
+                    border: "none",
+                }}
+                allow="fullscreen"
+                loading="lazy"
+                title="PowerApps"
+            />
         </div>
-    )
-}
+    );
+};
 
-export default Youtube
+export default Customize;
